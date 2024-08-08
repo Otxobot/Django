be54d03510d1b9d8757d1fed9c5d852ce9094f67
Aiert Basante <otxoboy64@gmail.com>
2024-08-08 17:59:48 +0200

    tienes que conectar la base de datos, esta levantado en su contenedor

--- ft_trascendence/settings.py
+++ ft_trascendence/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
 ]
 
 MIDDLEWARE = [
@@ -74,12 +75,16 @@ WSGI_APPLICATION = 'ft_trascendence.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         "PASSWORD": "postgress",
+#         "HOST": "0.0.0.0",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
