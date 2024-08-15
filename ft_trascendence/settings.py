5e5a6d93d7bc1c8f125d915f2add1ebf665eb632
Aiert Basante <otxoboy64@gmail.com>
2024-08-15 18:12:25 +0200

    la base de datos esta conectado

--- ft_trascendence/settings.py
+++ ft_trascendence/settings.py
@@ -75,17 +75,16 @@ WSGI_APPLICATION = 'ft_trascendence.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         "PASSWORD": "postgress",
-#         "HOST": "0.0.0.0",
-#         "PORT": "5432",
-#     }
-# }
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
